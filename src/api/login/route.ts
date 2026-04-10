a4881fd297657ac0875a57809a578794e507cdb3
a01722831 <149532767+a01722831@users.noreply.github.com>
2026-04-10 17:43:58 -0600

    Updated Dev with david api changes

---
+++ src/api/login/route.ts
@@ -0,0 +1,20 @@
+import bcrypt from "bcryptjs";
+import oracledb from "oracledb";
+type login = {
+    user: string,
+    password: string
+};
+
+export async function POST(request: Request){
+
+    const body: login = await request.json();
+    const user = body.user;
+    const password = body.password;
+
+    const res = await fetch(`https://g53bc679c5acb2c-espinabd.adb.mx-queretaro-1.oraclecloudapps.com/ords/admin/user/login?userName=${user}`);
+
+    const result = await res.json();
+    const hashToCompare = result.items[0].password_hash;
+    const isValid = (hashToCompare) ? await bcrypt.compare(password, hashToCompare) : false;
+    return Response.json(isValid);
+}
