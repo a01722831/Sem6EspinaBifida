2860d1c01f7132527460694006bd662fb7bcb07b
DavidGuerreroAragonA01285837 <a01285837@tec.mx>
2026-04-11 20:16:24 -0600

    Cambios salida endpoint obtener rutas
    Se cambio la salida del endpoint de obtener ruta de un objeto con una lista de consultas y una de estudios a una sola lista con ambos tipos de servicio ordenados por id.

--- appespinabifida/app/api/servicios/obtener/route.ts
+++ appespinabifida/app/api/servicios/obtener/route.ts
@@ -1,5 +1,34 @@
+import { serialize } from "v8";
 import * as obtenerConsultas from "./consultas/route";
 import * as obtenerEstudios from "./estudios/route";
+
+type estudio_data = {
+    id_estudio: number,
+    id_asociado: number,
+    id_medico: number,
+    id_tipo_estudio: number,
+    laboratorio: string,
+    aportacion: number,
+    ya_aporto: 1 | 0,
+    fecha_cita: string,
+    estatus: string,
+    resultados: string
+}
+
+type consulta_data = {
+    id_consulta: number,
+    id_asociado: number,
+    id_medico: number,
+    id_estudio: number,
+    id_recibo: number,
+    tipo_consulta: "primera" | "seguimiento",
+    motivo: string,
+    diagnostico: string,
+    tratamiento: string,
+    aportacion: number,
+    ya_aporto: 1 | 0,
+    estatus: string
+}
 
 export async function GET(){
     const res = await obtenerConsultas.GET();
@@ -9,6 +38,26 @@ export async function GET(){
     const result2 = await res2.json();
     const listaConsultas = result;
     const listaEstudios = result2;
-    const response = {consultas: listaConsultas, estudios: listaEstudios}
+    const servicios: (consulta_data | estudio_data)[] = [...listaConsultas, ...listaEstudios];
+
+    servicios.sort((a,b) => {
+        let a_val;
+        let b_val;
+        if ("id_consulta" in a){
+            a_val = a.id_consulta;
+        }
+        else{
+            a_val = a.id_estudio;
+        }
+        if ("id_consulta" in b){
+            b_val = b.id_consulta;
+        }
+        else{
+            b_val = b.id_estudio;
+        }
+        return a_val - b_val;
+    })
+
+    const response = {servicios};
     return Response.json(response);
 }
