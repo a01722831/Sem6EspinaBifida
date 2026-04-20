a9a1a93aa085d59f41837313aa6259bb773e432e
DavidGuerreroAragonA01285837 <a01285837@tec.mx>
2026-04-20 16:21:07 -0600

    fixes extras

--- appespinabifida/app/api/servicios/obtener/estudios/porId/route.ts
+++ appespinabifida/app/api/servicios/obtener/estudios/porId/route.ts
@@ -1,6 +1,12 @@
 export async function GET(request: Request){
     const { searchParams } = new URL(request.url);
-    const res = await fetch(`https://g53bc679c5acb2c-espinabd.adb.mx-queretaro-1.oraclecloudapps.com/ords/admin/services/obtenerEstudioPorId?id=${searchParams.get("id")}`);
+    const res = await fetch(`https://g53bc679c5acb2c-espinabd.adb.mx-queretaro-1.oraclecloudapps.com/ords/admin/services/obtenerEstudioPorId?id=${searchParams.get("id")}`,{
+        method: "GET",
+        headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Basic " + Buffer.from(`${process.env.DB_USER}:${process.env.DB_PASSWORD}`).toString("base64"),
+        }
+    });
     if (!res.ok) {
         return new Response(JSON.stringify({ error: "Error al obtener estudio" }), { status: res.status, headers: { "Content-Type": "application/json" } });
     }
